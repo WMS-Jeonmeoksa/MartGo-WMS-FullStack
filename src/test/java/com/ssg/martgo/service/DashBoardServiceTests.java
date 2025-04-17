85e4e4a0da98edfa30a1db15b87a5e3f9cfc78c8
KangChangSeon <rlckdtjs@naver.com>
2025-04-17 16:39:06 +0900

    for merged commit

---
+++ src/test/java/com/ssg/martgo/service/DashBoardServiceTests.java
@@ -0,0 +1,26 @@
+package com.ssg.martgo.service;
+
+import com.ssg.martgowmsfullstack.service.DashBoardService;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@Log4j2
+@ExtendWith(SpringExtension.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = {
+        "file:src/main/webapp/WEB-INF/spring/root-context.xml",
+        "file:src/main/webapp/WEB-INF/spring/servlet-context.xml"
+})
+public class DashBoardServiceTests {
+
+    private DashBoardService dashBoardService;
+
+    @Test
+    public void testDashBoardService() {
+        log.info(dashBoardService.getDashBoard("admin01"));
+    }
+}
