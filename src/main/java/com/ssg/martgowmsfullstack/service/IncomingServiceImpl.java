bed1411d1fe7231888b661757b9a94fe0202e6cd
sminseong <star919m@gmail.com>
2025-04-18 15:48:23 +0900

    refactor: incoming, outgoing, product ResultMap 생성 DTO와 데이터베이스 컬럼명 맞추기위해서 ResultMap 생성

--- src/main/java/com/ssg/martgowmsfullstack/service/IncomingServiceImpl.java
+++ src/main/java/com/ssg/martgowmsfullstack/service/IncomingServiceImpl.java
@@ -1,9 +1,15 @@
 package com.ssg.martgowmsfullstack.service;
 
+import com.ssg.martgowmsfullstack.domain.ProductVO;
+import com.ssg.martgowmsfullstack.dto.IncomingDTO;
+import com.ssg.martgowmsfullstack.dto.ProductDTO;
 import com.ssg.martgowmsfullstack.mapper.IncomingMapper;
+import com.ssg.martgowmsfullstack.mapper.ProductMapper;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,9 +17,35 @@ public class IncomingServiceImpl implements IncomingService {
 
     private final ModelMapper modelMapper;
     private final IncomingMapper incomingMapper;
+    private final ProductMapper productMapper;
 
     @Override
     public int getIncomingCount(String adminId) {
         return incomingMapper.getIncomingCountByAdminId(adminId);
+    }
+
+    @Override
+    public List<ProductDTO> getProductByUserId(String userId) {
+        return productMapper.getProductByUserId(userId);
+    }
+
+    @Override
+    public void requestIncoming(IncomingDTO incomingDTO) {
+
+    }
+
+    @Override
+    public List<IncomingDTO> getIncomingByRole(String adminId, String role) {
+        return List.of();
+    }
+
+    @Override
+    public void approveIncoming(String adminId, int incomingNum, String role) {
+
+    }
+
+    @Override
+    public String getAdminRoleById(String adminId) {
+        return "";
     }
 }
