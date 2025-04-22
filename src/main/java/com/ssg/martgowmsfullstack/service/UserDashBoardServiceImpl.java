a39098017a305a1b221245ac224c9b20403a6164
KangChangSeon <rlckdtjs@naver.com>
2025-04-22 12:34:21 +0900

    refactor : VO -> DTO 변경 수정
    VO 반환 타입으로 변경하고 Service 에서 DTO 로 변경하게끔 구현

--- src/main/java/com/ssg/martgowmsfullstack/service/UserDashBoardServiceImpl.java
+++ src/main/java/com/ssg/martgowmsfullstack/service/UserDashBoardServiceImpl.java
@@ -1,10 +1,19 @@
 package com.ssg.martgowmsfullstack.service;
 
+import com.ssg.martgowmsfullstack.domain.RentSectorWarehouseVO;
+import com.ssg.martgowmsfullstack.domain.SectorUsageVO;
+import com.ssg.martgowmsfullstack.domain.UserAdminVO;
 import com.ssg.martgowmsfullstack.dto.DashBoardDTO;
+import com.ssg.martgowmsfullstack.dto.RentSectorWarehouseDTO;
+import com.ssg.martgowmsfullstack.dto.SectorUsageDTO;
+import com.ssg.martgowmsfullstack.dto.UserAdminDTO;
 import com.ssg.martgowmsfullstack.mapper.DashBoardMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,12 +24,39 @@ public class UserDashBoardServiceImpl implements UserDashBoardService {
 
     @Override
     public DashBoardDTO getDashBoard(String user_id) {
+        List<RentSectorWarehouseVO> rentSectorWarehouseVOList = dashBoardMapper.getRentSectorWarehouseList(user_id);
+        List<RentSectorWarehouseDTO> rentSectorWarehouseDTOList = rentSectorWarehouseVOList.stream()
+                .map(vo -> RentSectorWarehouseDTO.builder()
+                        .sector_id(vo.getSector_id())
+                        .warehouse_name(vo.getWarehouse_name())
+                        .build())
+                .collect(Collectors.toList());
+
+        List<UserAdminVO> userAdminVOList = dashBoardMapper.getUserAdminList(user_id);
+        List<UserAdminDTO> userAdminDTOList = userAdminVOList.stream()
+                .map(vo -> UserAdminDTO.builder()
+                        .admin_name(vo.getAdmin_name())
+                        .phone_num(vo.getPhone_num())
+                        .build())
+                .collect(Collectors.toList());
+
+        List<SectorUsageVO> userSectorUsageVOList = dashBoardMapper.getUserSectorUsage(user_id);
+        List<SectorUsageDTO> userSectorUsageDTOList = userSectorUsageVOList.stream()
+                .map(vo -> SectorUsageDTO.builder()
+                        .sector_id(vo.getSector_id())
+                        .warehouse_id(vo.getWarehouse_id())
+                        .FAR(vo.getFAR())
+                        .build())
+                .collect(Collectors.toList());
+
         DashBoardDTO dashBoardDTO = DashBoardDTO.builder()
                 .remainingDays(dashBoardMapper.getRemainingDays(user_id))
-                .rentSectorWarehouseList(dashBoardMapper.getRentSectorWarehouseList(user_id))
-                .userAdminList(dashBoardMapper.getUserAdminList(user_id))
-                .userSectorUsage(dashBoardMapper.getUserSectorUsage(user_id))
+                .rentSectorWarehouseList(rentSectorWarehouseDTOList)
+                .userAdminList(userAdminDTOList)
+                .userSectorUsage(userSectorUsageDTOList)
                 .build();
+
         return dashBoardDTO;
     }
+
 }
