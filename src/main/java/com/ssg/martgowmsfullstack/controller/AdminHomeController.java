f55c5694e972117e9cee334ac83ef1c57a3bdb98
portsungbin <sd61224@gmail.com>
2025-04-23 17:14:52 +0900

    refactor/ login
    권한별 URL이동 예외처리

--- src/main/java/com/ssg/martgowmsfullstack/controller/AdminHomeController.java
+++ src/main/java/com/ssg/martgowmsfullstack/controller/AdminHomeController.java
@@ -2,6 +2,7 @@ package com.ssg.martgowmsfullstack.controller;
 
 import javax.servlet.http.HttpSession;
 
+import com.ssg.martgowmsfullstack.dto.AdminDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,8 +23,16 @@ public class AdminHomeController {
     // 창고 관리자 마이페이지
     @GetMapping("/mypage")
     public String mypage(HttpSession session) {
-        if (session.getAttribute("loginInfo") == null) {
+        Object loginInfo = session.getAttribute("loginInfo");
+        if (loginInfo == null) {
             return "redirect:/login";
+        }
+        if (!(loginInfo instanceof AdminDTO)) {
+            return "redirect:/access-denied";
+        }
+        AdminDTO adminDTO = (AdminDTO) loginInfo;
+        if (!"창고관리자".equals(adminDTO.getRole())) {
+            return "redirect:/access-denied";
         }
         return "admin-mypage"; // /WEB-INF/views/warehouseadmin-mypage.jsp
     }
